6810a7a875f6f26c216c06618d214ffbda402bbe
Pavel Medvedev <pmedvedev@gmail.com>
2016-10-06 15:55:04 +0300

    Add required_modules container to store loaded dependency modules
    Implement required_modules member functions as inline to use it when this
    module is used a delay-loaded dll

--- include/nitrogen/nodeutil.hpp
+++ include/nitrogen/nodeutil.hpp
@@ -53,12 +53,46 @@ void call_in_node(F&& f)
 inline v8::Handle<v8::Value> require(v8::Handle<v8::Object> module, char const* name)
 {
 	v8::Isolate* isolate = module->GetIsolate();
-	v8::HandleScope scope(isolate);
+	v8::EscapableHandleScope scope(isolate);
 	v8::Local<v8::Value> require_name = v8::String::NewFromUtf8(isolate, "require");
 	v8::Local<v8::Value> module_name = v8::String::NewFromUtf8(isolate, name);
 	v8::Local<v8::Function> require = module->Get(require_name).As<v8::Function>();
-	return require->Call(module, 1, &module_name);
+	v8::Local<v8::Value> result = require->Call(module, 1, &module_name);
+	return scope.Escape(result);
 }
+
+/// List of persistent handles to loaded modules
+class required_modules
+{
+public:
+	required_modules() = default;
+	required_modules(required_modules const&) = delete;
+	required_modules& operator=(required_modules const&) = delete;
+	~required_modules() { unload(); }
+
+	/// Load list of modules
+	void required_modules::load(v8::Handle<v8::Object> module, std::initializer_list<char const*> names)
+	{
+		unload();
+		modules_.reserve(names.size());
+		for (auto name : names)
+		{
+			modules_.emplace_back(module->GetIsolate(), require(module, name));
+		}
+	}
+
+	/// Unload all modules
+	void required_modules::unload()
+	{
+		std::for_each(modules_.rbegin(), modules_.rend(),
+			[](persistent& module) { module.Reset(); });
+		modules_.clear();
+	}
+
+private:
+	using persistent = v8::UniquePersistent<v8::Value>;
+	std::vector<persistent> modules_;
+};
 
 /// Event emitter
 class NITROGEN_API event_emitter
